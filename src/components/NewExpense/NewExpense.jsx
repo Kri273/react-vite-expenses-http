55d37e01c26c69b82de7d1e5e136e982e5ae9e6a
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-11-21 17:30:48 +0200

    expense form adding

---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,12 @@
+import ExpenseForm from "./ExpenseForm"
+
+const NewExpense = (props) => {
+    return(
+        <div className="new-expense">
+            <ExpenseForm/>
+        </div>
+
+    )
+}
+
+export default NewExpense
